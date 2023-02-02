d8d3efbcf1c52a1720703d6f58b9bcd2e5547565
DemakovAndrii <demakov1155@gmail.com>
2023-02-02 14:53:34 +0200

    add persist

--- src/Store/MutationStore.js
+++ src/Store/MutationStore.js
@@ -1,25 +1,28 @@
-import produce from "immer";
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
+import { persist } from "zustand/middleware";
 
 export const useCountStore = create()(
-	immer((set) => ({
-		arr: [
-			{ id: 1, text: "text" },
-			{ id: 2, text: "text" },
-			{ id: 3, text: "text" },
-		],
-		add: (text) =>
-			set((state) => {
-				state.arr.push({ text });
-			}),
-		del: (index) => {
-			set(
-				produce((state) => {
-					const indexOfCount = state.arr.findIndex((el) => el.id === index);
+	persist(
+		immer((set) => ({
+			arr: [
+				{ id: 1, text: "text" },
+				{ id: 2, text: "text" },
+				{ id: 3, text: "text" },
+				{ id: 4, text: "text" },
+				{ id: 5, text: "text" },
+			],
+			add: (text) =>
+				set((state) => {
+					state.arr.push({ id: Date.now(), text });
+				}),
+			del: (id) => {
+				set((state) => {
+					const indexOfCount = state.arr.findIndex((el) => el.id === id);
 					state.arr.splice(indexOfCount, 1);
-				})
-			);
-		},
-	}))
+				});
+			},
+		})),
+		{ name: "testStore", vertion: 1 }
+	)
 );
